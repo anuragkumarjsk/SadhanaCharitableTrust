b36c193ef1547092431f1c62895fdc236bd46ae1
komalnagaraj <komalnagaraj5@gmail.com>
2022-10-18 23:36:53 +0530

    Responsive NavBar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
-
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-
-import TopNavbar from './Pages/TopNavbar';
 import Home from './Pages/Home';
 import Activities from './Pages/Activities';
 import Donate from './Pages/Donate';
 import Volunteer from './Pages/Volunteer';
 import ContactUs from './Pages/ContactUs';
+import NavBar from './Pages/NavBar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <TopNavbar/>
+        <NavBar/>
         <Routes>
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/activities" element={<Activities />}/>
---
+++ src/Pages/NavBar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import logo from '../Images/logo.jpg'
+import {FaFacebookSquare} from "react-icons/fa"
+import './PagesCss/navbar.css'
+
+function NavBar() {
+  return (
+    <>
+    <nav className='main-nav'>
+      <h4 className='org-name'>Sadhana Charitable Trust</h4>
+      <div className='logo'>
+        <a href='/'><img  alt='logo' className='logo-img' rel="apple-touch-icon" align='left' src={logo} href='../../public/logo.jpg' /></a> 
+      </div>
+      <div className='trust-name'>
+        <h2 >Sadhana Charitable Trust</h2>
+      </div>
+      <div className='menu-link'>
+        <ul>
+          <li><a href='/' className='active'>Home</a></li>
+          <li><a href='/activities'>Activities</a></li>
+          <li><a href='/donate'>Donate</a></li>
+          <li><a href='/volunteer'>Volunteer</a></li>
+          <li><a href='/contactus'>Contact Us</a></li>
+        </ul>
+      </div>
+        {/*Social media links*/}
+        <div className='social-media'>
+            <ul className='social-media-desktop'>
+                <li><a href='https://www.facebook.com/people/Sadhana-Charitable-Trust/100067687247213/' ><FaFacebookSquare className='facebook'/> Follow on FB</a></li>
+            </ul>
+        </div>
+    </nav>
+    <section className='hero-section'>
+        <h1>Welcome to Sadhana Charitable Trust</h1>
+    </section>
+  </>
+  )
+}
+
+export default NavBar
