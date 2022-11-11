7e37293abd5fb5c8e61a7b718eaf34f60aad6a45
komalnagaraj <komalnagaraj5@gmail.com>
2022-11-11 22:11:34 +0530

    home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,17 +4,42 @@ import './PagesCss/home.css'
 function Home() {
   const [teacher, setTeacher] = useState(0);
   const [students, setStudents] = useState(0);
+  const [volunteers, setVolunteer] = useState(0);
+  const [awards, setAward] = useState(0);
+  const Count = [{'teacherNo':20, 'teacherVar':teacher, 'teacherFun': setTeacher},]
 
-  const increaseCount = useEffect(() =>{
-    for(let i=0;i<30;i++){
-      setTeacher(teacher+1)}
-    return ()=> teacher
-  })
+  useEffect(() =>{
+    let count = teacher<20 &&  setInterval(()=>{
+      setTeacher(prevCount => {return prevCount+1})
+    },100)     
+    return () => clearInterval(count)  
+  },[teacher])
+
+  useEffect(() =>{
+    let studentCount = students < 200 && setInterval(()=>{
+      setStudents(prevCount => {return prevCount+1})
+    },10)  
+    return () => clearInterval(studentCount)
+  },[students])
+
+  useEffect(() =>{
+    let volunteerCount = volunteers < 100 && setInterval(()=>{
+      setVolunteer(prevCount => {return prevCount+1})
+    },10)  
+    return () => clearInterval(volunteerCount)
+  },[volunteers])
+
+  useEffect(() =>{
+    let awardCount = volunteers < 50 && setInterval(()=>{
+      setAward(prevCount => {return prevCount+1})
+    },40)  
+    return () => clearInterval(awardCount)
+  },[awards])
 
   return (
     
     <div >
-      <div class="jumbotron" style={{
+      <div className="jumbotron" style={{
         backgroundImage: `url(${backImage})`,
         backgroundAttachment:'fixed'
        }}>
@@ -26,22 +51,20 @@ function Home() {
               fontSize:'50px'}}>
           <b>SADHANA CHARITABLE TRUST</b>
           </h1>
-          
-          
         
       </div>
       <div className='info-card'>
-       <div className='info' onLoad={increaseCount}>
+       <div className='info'>
         <p><h2>{teacher}+</h2>Teachers Working</p>
        </div>
        <div className='info'>
-       <p><h2>200+</h2>Students at the trust</p>
+       <p><h2>{students}+</h2>Students at the trust</p>
        </div>
        <div className='info'>
-       <p><h2>100+</h2>Volunteers working with us</p>
+       <p><h2>{volunteers}+</h2>Volunteers working with us</p>
        </div>
        <div className='info'>
-       <p><h2>50+</h2>Awards received</p>
+       <p><h2>{awards}+</h2>Awards received</p>
        </div>
       </div>
     </div>
